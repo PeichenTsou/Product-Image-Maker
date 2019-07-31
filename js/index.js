82697d50185cbade8057075c2758c0b70845a468
Peggy <q450052000@gmail.com>
2019-07-31 15:28:14 +1000

    Tidy up the code and revise the layout

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 // <!-- Configure a few settings and attach camera -->
 Webcam.set({
-  // width: 780, //手機版才有的畫質
+  // width: 780, // phone camera quality
   // height: 1280,
-  width: 320, //電腦版測試用
+  width: 320, // Computer camera quality
   height: 640,
   image_format: 'png',
   // jpeg_quality: 90
@@ -21,37 +21,32 @@ function take_snapshot() {
   });
 }
 
-///HTML轉為圖檔 ((迴圈版))
+///Change HTML to picture file  ((Loop ver.))
 $("#changeToPic").click(function() {
   for(var i = 0; i < $(".iWantCapture").length; i++){
-    console.log("HTML轉為圖檔"+i);
-  
-    html2canvas($(".iWantCapture")[i], {scale:2,allowTaint:true})
+    console.log("Change HTML to picture"+i);
+
+    html2canvas($(".iWantCapture")[i], {scale:1,allowTaint:true})
       .then(function(canvas){
-        var $div = $("fieldset div"); //fieldset裡面的div
+        var $div = $("fieldset div"); //div inside fieldset
         var $canvas = canvas;
-    
+
         console.log("$canvas = canvas"+$canvas.toDataURL("image/png"));
         $("<img />", { src: $canvas.toDataURL("image/png") }).appendTo($div);          
         var url = $canvas.toDataURL("image/png");
     
-        //把網址加到陣列
+        //Adding URL into Array
         mydata.downloadLinks.length = 0;
         mydata.downloadLinks.push(canvas.toDataURL("image/png"));
         
-        var link = $("<a href='#' download='dl.png' class='oneDownloadLink'>單張下載</a>").attr('href', mydata.downloadLinks[0]);
+        var link = $("<a href='#' download='dl.png' class='oneDownloadLink'>Download one picture</a>").attr('href', mydata.downloadLinks[0]);
         $("fieldset div").append(link);
       });
   }
 });
 
-//一次下載多張
-$('#downloadMany2').click(function() {
-  $('.oneDownloadLink').click()
-  console.log("downloadMany"+1);
-});
 
-//取日期的值
+//Get Date value
 var date = $("#datepicker").val();
 
 var mydata={
@@ -71,29 +66,29 @@ $(".culcu_btn").click(
   function calcuPrice() {
     console.log(1);
     var total_html="<div id='after'>NT${{priceAfterCalc}}</div>";
-    $("#after").remove(); //刪除ATER↑不能刪除#priceDiv
+    $("#after").remove(); //Delete ATER↑ But can't delete #priceDiv
     
-    //計算台幣價格
+    //Calculate the price in NT$
     var after_price = 0.027*parseInt(mydata.originalPrice)+parseInt(mydata.weight)*parseInt(mydata.delivery_Per1kg_NT)+parseInt(mydata.adjust);
     var current_total_html= total_html.replace("{{priceAfterCalc}}",after_price);
     $(".priceDiv").append(current_total_html);
 
-    //取datepicker值把日期放進去
-    mydata.dueDate = $("#datepicker").val()
+    //Get datepicker value and put the date into it
+    mydata.dueDate = $("#datepicker").val();
 
-    //讓浮水印圖片出現 http://www.cnblogs.com/smallmuda/archive/2010/11/30/1892384.html
+    //Show the watermark http://www.cnblogs.com/smallmuda/archive/2010/11/30/1892384.html
     $(".waterimg").css('display','block');
   }    
 );
 
-///日期選擇
+///Data selection
 $(function() {
   $("#datepicker").datepicker();
   $("#anim").change(function() {
     $("#datepicker").datepicker("option", "showAnim", $( this ).val());
   });
   
-  //設定默認日期與格式　http://hant.ask.helplib.com/jquery-ui/post_970654
+  //Set default date and format　http://hant.ask.helplib.com/jquery-ui/post_970654
   $("#datepicker").datepicker("option","dateFormat","yy/mm/dd");
   $('#datepicker').datepicker('setDate', new Date());
 });
@@ -103,9 +98,9 @@ var vm=new Vue({
   data: mydata
 });
 
-/////上傳多個圖片預覽
+/////Previewing multiple pictures uploaded
 $("#progressbarTWInput").change(function(){
-  $("#preview_progressbarTW_imgs").html(""); // 清除預覽
+  $("#preview_progressbarTW_imgs").html(""); // clear the preview
   readURL(this);
 });
 
@@ -118,10 +113,10 @@ function readURL(input) {
       reader.onload = function (e) {
         console.log("function (e)" + i); //test
         
-        ///把資訊加上去
-        var info_html="<div class='iWantCapture' id='app2'><div class='waterimg'>浮水印字樣</div><div  id='results2' class='scrshot2'><img src='{{imgsrc}}' height='100px'/></div><table><tr><td>{{productName}}</td></tr><tr><td><div class='priceDiv2'>NT${{priceAfterCalc}}</div></td></tr><tr><td>【收單時間{{dueDate}}, {{dueTime}}】</div></td></tr></table></div>";
+        ///Add the info
+        var info_html="<div class='iWantCapture' id='app2'><div class='waterimg'>浮水印字樣</div><div  id='results2' class='scrshot2'><img src='{{imgsrc}}' height='100px'/></div><table><tr><td>{{productName}}</td></tr><tr><td><div class='priceDiv2'>NT${{priceAfterCalc}}</div></td></tr><tr><td>【Due Date: {{dueDate}}, {{dueTime}}】</div></td></tr></table></div>";
         
-        //計算台幣價格
+        //Calculate price in NT$
         var after_price = 0.027 * parseInt(mydata.originalPrice) + parseInt(mydata.weight) * parseInt(mydata.delivery_Per1kg_NT) + parseInt(mydata.adjust);
         var new_htm2= info_html.replace("{{imgsrc}}",e.target.result)
           .replace("{{productName}}",mydata.productName)
@@ -131,7 +126,7 @@ function readURL(input) {
 
         $("#preview_progressbarTW_imgs").append(new_htm2);
         
-         //讓浮水印圖片出現
+         //Show the watermark
         $(".waterimg").css('display','block');
       }
 
@@ -139,7 +134,7 @@ function readURL(input) {
       reader.readAsDataURL(input.files[i]);
     }
   }else{
-     var noPictures = $("<p>目前沒有圖片</p>");
+     var noPictures = $("<p>There is no pictures now.</p>");
      $("#preview_progressbarTW_imgs").append(noPictures);
   }
 }
